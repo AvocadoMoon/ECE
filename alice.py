d7cab7e3907ad749bfb4a9459bb7fc54437179fd
Julia-Guskind <57551101+Julia-Guskind@users.noreply.github.com>
2021-12-23 17:37:20 -0500

    baseline: protocol design and sockets

---
+++ alice.py
@@ -0,0 +1,40 @@
+import socket
+
+# Alice is acting as the "server"
+
+IP = "127.0.0.1"    # IP address points to ourself
+PORT = 3400         # completely random port number
+
+#AF_INET means IPV4
+#SOCK_STREAM means connection-oriented TCP protocol
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create the socket object, uses a TCP connection
+sock.bind((IP, PORT))   # bind to the specified IP address and port number
+sock.listen(3)          # prepare to listen for incoming connections
+
+def handleClientConnection(clientsock, addr):
+    while True:
+        output = input()    # get user input from alice
+        clientsock.send(output.encode())    # send a hello message to the client as a byte string
+        if output == "q":   # if alice wants to quit, then quit
+            clientsock.close()
+            break
+        response = clientsock.recv(1024).decode()   # receive a reply from the client and convert it back to a regular string
+        if response == "q":
+            clientsock.close()  # end the socket connection with our client
+            break               # break out of the loop
+        else:
+            print("Client: ", response) 
+            
+
+clientsock, addr = sock.accept()    # accept a connection from a client
+handleClientConnection(clientsock, addr)
+
+
+# References;
+# https://www.geeksforgeeks.org/socket-programming-python/
+# https://realpython.com/python-sockets/ 
+
+
+
+
---
+++ bob.py
@@ -0,0 +1,21 @@
+import socket
+import random
+
+IP = "127.0.0.1"    # IP address points to ourself
+PORT = 3400         # completely random port number
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create the socket object, uses a TCP connection
+sock.connect((IP, PORT))    # connect to the server
+
+while True:
+    msg = sock.recv(1024).decode()    # receive a message from the server
+    print("Server: ", msg)
+    if msg == "q":
+        sock.close()
+        break
+
+    output = input()
+    sock.send(output.encode()) # send a message to the server
+    if output == "q":
+        sock.close()
+        break
