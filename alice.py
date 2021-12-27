84d0737665544c36dbbf59f5c1652d6c4592f02f
EmuWarfare <68507214+EmuWarfare@users.noreply.github.com>
2021-12-27 16:29:49 -0500

    Object Oriented Implementation
    Further implementing the object oriented version of communication. Includes multithreading so Alice and Bob can send multiple messages at once. As well as receive multiple messages at once.

--- alice.py
+++ alice.py
@@ -1,4 +1,5 @@
 import socket
+import protocol
 
 # Alice is acting as the "server"
 
@@ -8,27 +9,31 @@ PORT = 3400         # completely random port number
 #AF_INET means IPV4
 #SOCK_STREAM means connection-oriented TCP protocol
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create the socket object, uses a TCP connection
-sock.bind((IP, PORT))   # bind to the specified IP address and port number
-sock.listen(3)          # prepare to listen for incoming connections
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create the socket object, uses a TCP connection
+# sock.bind((IP, PORT))   # bind to the specified IP address and port number
+# sock.listen(3)          # prepare to listen for incoming connections
 
-def handleClientConnection(clientsock, addr):
-    while True:
-        output = input()    # get user input from alice
-        clientsock.send(output.encode())    # send a hello message to the client as a byte string
-        if output == "q":   # if alice wants to quit, then quit
-            clientsock.close()
-            break
-        response = clientsock.recv(1024).decode()   # receive a reply from the client and convert it back to a regular string
-        if response == "q":
-            clientsock.close()  # end the socket connection with our client
-            break               # break out of the loop
-        else:
-            print("Client: ", response) 
+# def handleClientConnection(clientsock, addr):
+#     while True:
+#         output = input()    # get user input from alice
+#         clientsock.send(output.encode())    # send a hello message to the client as a byte string
+#         if output == "q":   # if alice wants to quit, then quit
+#             clientsock.close()
+#             break
+#         response = clientsock.recv(1024).decode()   # receive a reply from the client and convert it back to a regular string
+#         if response == "q":
+#             clientsock.close()  # end the socket connection with our client
+#             break               # break out of the loop
+#         else:
+#             print("Client: ", response) 
             
 
-clientsock, addr = sock.accept()    # accept a connection from a client
-handleClientConnection(clientsock, addr)
+# clientsock, addr = sock.accept()    # accept a connection from a client
+# handleClientConnection(clientsock, addr)
+
+alice = protocol.Server(3400, "127.0.0.1", "Alice")
+
+alice.connectClient()
 
 
 # References;
--- protocol.py
+++ protocol.py
@@ -3,15 +3,45 @@ import threading
 
 #due to similarity in how client and server recieve messages, just create a class for both
 class Transportation():
+    def __init__(self, selfName):
+        self.selfName = selfName
+        self.othersName = None
+
     def textMessage(self, sock):
-        def sendMessage():
+        def sendMessage(sock, selfName):
+            while True:
+                output = input()
+                if output == "q":
+                    sock.send((selfName + " Has Left").encode())
+                    break
+                sock.send(output.encode())
+
+
+        def recieveMessage(sock, othersName):
+            while True:
+                msg = sock.recv(1024).decode()
+                if (msg == "q"):
+                    break
+                print(othersName + ": " + msg)
+    
+        send = threading.Thread(target=sendMessage, args=(sock, self.selfName))
+        recieve = threading.Thread(target=recieveMessage, args=(sock, self.othersName, ))
+
+        send.start()
+        recieve.start()
+
+        recieve.join()
+        send.join
+
+        sock.close()
+    
+
+    def fileTransfer(self, sock):
+        def sendFile(self, sock):
             pass
 
-        def recieveMessage():
+        def recieveFile(self, sock):
             pass
-
-    def fileTransfer(self):
-        pass
 
 class Client():
     def __init__(self, PORT, IP, name):
@@ -19,7 +49,7 @@ class Client():
         self.PORT = PORT
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create the socket object, uses a TCP connection
         self.name = name
-        self.transport = Transportation()
+        self.transport = Transportation(name)
         if (type(self.name) != type(" ")):
             raise TypeError
     
@@ -28,6 +58,8 @@ class Client():
         self.sock.send(self.name.encode())      #in future already have encryption set up and message through that
         response = self.sock.recv(1024).decode()
         if (response == "Y"):
+            serverName = self.sock.recv(1024).decode()
+            self.transport.othersName = serverName
             self.transport.textMessage(self.sock)
 
     
@@ -37,24 +69,24 @@ class Server():
         self.IP = IP
         self.PORT = PORT
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create the socket object, uses a TCP connection
-        self.clientSock = None
-        self.addr = None
         self.name = name
-        self.transport = Transportation()
+        self.transport = Transportation(name)
         if (type(self.name) != type(" ")):
             raise TypeError
     
     def connectClient(self):
         self.sock.bind((self.IP, self.PORT))   # bind to the specified IP address and port number
         self.sock.listen(3)          # prepare to listen for incoming connections
-        self.clientsock, self.addr = self.sock.accept()    # accept a connection from a client
+        clientSock, addr = self.sock.accept()    # accept a connection from a client
 
-        name = self.clientsock.recv(1024).decode()      #no way to authenticate clients name yet
-        msg = input("Do you want to communicate with: " + name + "? (Y/N)") #allow more options other than just text based communication in the future
+        name = clientSock.recv(1024).decode()      #no way to authenticate clients name yet
+        self.transport.othersName = name
+        msg = input("Do you want to communicate with: " + name + "? (Y/N) ") #allow more options other than just text based communication in the future
         if (msg == "Y"):
-            self.clientSock.send("Y".encode())
-            self.transport.textMessage(self.clientSock)
-        self.clientSock.send("No".encode())
+            clientSock.send("Y".encode())
+            clientSock.send(self.name.encode())
+            self.transport.textMessage(clientSock)
+        clientSock.send("No".encode())
     
 
 
